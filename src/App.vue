5cc9a60521ff9c62b7bc1b8471747f0bd611c419
Fabio Castillo <fabiocastillo115@gmail.com>
2024-11-08 19:23:37 -0600

    Formulario de registro y login

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,36 @@
 <template>
-  <v-app>
+  <v-app>     
+    <v-app-bar color="deep-purple-darken-1" prominent >
+      <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Sistema de notas MIAURELIO</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon="mdi-home" to="/"></v-btn>
+    </v-app-bar>
+
+    <v-navigation-drawer color="deep-purple-darken-1" v-model="drawer" permanent>
+      <v-list>
+        <template  v-if="this.$store.getters.getUsuario==null">
+          <v-list-item link to="/" title="Login"  prepend-icon="mdi-login"></v-list-item>
+        </template>
+
+        <template v-if="this.$store.getters.getUsuario!==null">
+          <template v-if="this.$store.getters.getRol=='admin'">
+            <v-list-item link to="/admin" title="Inicio" prepend-icon="mdi-home"></v-list-item>
+            <v-list-item link to="/alumnos" title="Alumnos" prepend-icon="mdi-home"></v-list-item>
+            <v-list-item link to="/profesores" title="Profesores" prepend-icon="mdi-home"></v-list-item>
+            <v-list-item link to="/grados" title="Grados" prepend-icon="mdi-home"></v-list-item>
+            <v-list-item link to="/materias" title="Materias" prepend-icon="mdi-home"></v-list-item>
+          </template>
+          <template v-if="this.$store.getters.getRol!=='admin'">
+          </template>
+            <v-list-item link to="/" title="Inicio" prepend-icon="mdi-home"></v-list-item>
+            <v-list-item link to="/" title="Mis clases" prepend-icon="mdi-home"></v-list-item>
+            <v-list-item link to="/" title="Asistencia" prepend-icon="mdi-home"></v-list-item>
+            <v-list-item link to="/" title="Actividades" prepend-icon="mdi-home"></v-list-item>
+            <v-list-item link to="/" title="Notas" prepend-icon="mdi-home"></v-list-item>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
     <v-main>
       <router-view/>
     </v-main>
@@ -11,8 +42,42 @@
 export default {
   name: 'App',
 
-  data: () => ({
-    //
-  }),
+  data(){
+    return{
+      drawer: true,
+      group: null,
+    }
+  },
+  methods: {
+    // Este método realiza el cierre de sesión
+    logout(){
+      // Vaciando el estado de la aplicación y el storage
+      this.$store.dispatch('logout')
+      // Redireccionando al login
+      this.$router.push('/')
+    },
+    // Este método verifica si hay sesión para mantener o expulsar al usuario de la aplicación
+    validarAcceso(){
+      let datos = localStorage.getItem('userData');
+      if(datos){
+        // Si hay data en el storage se valida el acceso
+        this.$store.dispatch('login', JSON.parse(datos));
+        this.$router.push('/welcome')
+        console.log(datos)
+      } else {
+        // Si no hay data se redirecciona al login
+        this.$router.push('/')
+      }
+    }
+  },
+  created(){
+    // El método se ejecuta cada vez que el componente se crea
+    this.validarAcceso()
+  },
+  watch: {
+      group () {
+        this.drawer = false
+      },
+  }
 }
 </script>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,136 @@
+<template>
+    <v-container fluid>
+        <v-row justify="center">
+            <v-col xs="12" sm="4" md="4" lg="4" xl="4" xxl="4">
+                <v-card>
+                    <v-img
+                        height="150"
+                        cover
+                        src="https://www.salesforce.com/content/dam/web/es_mx/blog/seguimiento-de-ventas-como-hacer.jpg">
+                    </v-img>
+                    <v-card-text>
+                        <h2 class="text-blue-grey-darken-2">
+                            Sistema de Ventas
+                        </h2>
+                        <p class="text-blue-grey-darken-2">
+                            Login
+                        </p>
+                        <br>
+                        <v-form>
+                            <v-text-field
+                                label="Correo"
+                                placeholder="correo@ventaxperto.com"
+                                counter
+                                maxlength="70"
+                                color="indigo"
+                                clearable
+                                prepend-inner-icon="mdi-email"
+                                v-model="usuario.email"
+                            >
+                            <!-- AJUSTAR MODELOS  -->
+                            </v-text-field>
+                            <v-text-field
+                                label="Contraseña"
+                                placeholder="*********"
+                                counter
+                                maxlength="15"
+                                color="indigo"
+                                clearable
+                                type="password"
+                                prepend-inner-icon="mdi-key"
+                                v-model="usuario.password"
+                            >
+                            <!-- AJUSTAR MODELOS  -->
+                            </v-text-field>
+                            <v-btn
+                                color="indigo"
+                                block
+                                @click="login"
+                            >
+                                Iniciar sesión
+                            </v-btn>
+                            <br>
+                            <p class="text-blue-grey-darken-2 text-center">
+                                ¿No cuentas con un usuario?
+                            </p>
+                            <v-btn
+                                color="indigo"
+                                block
+                                to="/register"
+                            >
+                                Registrarse
+                            </v-btn>
+                        </v-form>
+                    </v-card-text>
+                </v-card>
+            </v-col>
+        </v-row>
+        <!-- Alerta -->
+        <v-snackbar
+            :timeout="1500"
+            color="red-darken-4"
+            v-model="alertaEstado"
+        >
+            {{ alertaMensaje }}
+        </v-snackbar>
+    </v-container>
+</template>
+
+<script>
+    // Importar axios
+    import axios from 'axios'
+
+    export default {
+        name: 'LoginView',
+        data(){
+            return {
+                // Variable de formulario
+                usuario: {
+                    // AJUSTAR VARIABLES CON NOMBRES DE CAMPOS EN LA BD
+                    email: '',
+                    password: ''
+                },
+                // Variables para controlar la alerta
+                alertaEstado: false,
+                alertaMensaje: ''
+            }
+        },
+        methods: {
+            // Petición para realizar login
+            login(){
+                axios.post('http://127.0.0.1:8000/api/usuario/login', this.usuario)
+                .then(response => {
+                    // console.log(response)
+                    if(response.status==200){
+                        // Se extrae el usuario y token de
+                        // la respuesta que retorna el endpoint de login
+                        let datos = {
+                            // AJUSTAR RESPONSE.DATA.DATA. CON NOMBRES DE CAMPOS EN LA BD
+                            usuario: response.data.data.name,
+                            email: response.data.data.email,
+                            token: response.data.token
+                        }
+                        // Guardando datos en el storage y el state
+                        this.$store.dispatch('login', datos)
+                        if (datos.rol == 'admin') {
+                            this.$router.push('/admin')
+                        } else {
+                            // Redireccionando a la pantalla de bienvenida
+                            this.$router.push('/welcome')
+                        }
+                    }
+                })
+                .catch(error => {
+                    console.log('Ha ocurrido un error '+error)
+                    this.alertaEstado = true
+                    // Usuario no autorizado
+                    if(error.response.status==401){
+                        this.alertaMensaje = error.response.data.data
+                    } else {
+                        this.alertaMensaje = '¡Ups! Algo salió mal'
+                    }
+                })
+            }
+        }
+    }
+</script>
